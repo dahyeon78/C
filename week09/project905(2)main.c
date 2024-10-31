f1ca38156fc444c1be4be2f4da2f02728fe3f028
dahyeon78 <daheon7872@gmail.com>
2024-10-31 22:45:04 +0900

    9주차
    실습 1,2,3,4,5-1,5-2

---
+++ week09/project905(2)main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+static int all_files;
+
+extern void sub();
+
+int main(int argc, char *argv[]) {
+	
+    sub();
+    printf("%d\n", all_files);
+ 
+	return 0;
+}
